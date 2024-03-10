ffa52e0853102d50b6ba29e6e33bea1294c1508f
diogotavc <diogotavc@protonmail.com>
2024-03-10 00:16:22 +0000

    [WIP] Preliminary ID and Forecast implementation

--- pkg/weather/weather.go
+++ pkg/weather/weather.go
@@ -1,17 +1,82 @@
 package weather
 
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+)
+
+type Location struct {
+	IdRegiao      int    `json:"idRegiao"`
+	IdAreaAviso   string `json:"idAreaAviso"`
+	IdConcelho    int    `json:"idConcelho"`
+	GlobalIdLocal int    `json:"globalIdLocal"`
+	Latitude      string `json:"latitude"`
+	IdDistrito    int    `json:"idDistrito"`
+	Local         string `json:"local"`
+	Longitude     string `json:"longitude"`
+}
+
+type Forecast struct {
+	PrecipitaProb  string `json:"precipitaProb"`
+	TMin           string `json:"tMin"`
+	TMax           string `json:"tMax"`
+	PredWindDir    string `json:"predWindDir"`
+	IdWeatherType  int    `json:"idWeatherType"`
+	ClassWindSpeed int    `json:"classWindSpeed"`
+	Longitude      string `json:"longitude"`
+	ForecastDate   string `json:"forecastDate"`
+	ClassPrecInt   int    `json:"classPrecInt"`
+	Latitude       string `json:"latitude"`
+}
+
+type IPMA struct {
+	Owner   string     `json:"owner"`
+	Country string     `json:"country"`
+	Data    []Location `json:"data"`
+}
+
 func Report(city string) string {
-	return ""
+	forecast := getWeatherForecast(getGlobalIdLocal(city), 1)
+	forecastJSON, err := json.Marshal(forecast)
+	checkError(err)
+	return string(forecastJSON)
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
 
 func getGlobalIdLocal(city string) int {
-	// grab id from https://api.ipma.pt/open-data/distrits-islands.json
-	// from a city string - example: Aveiro -> 1010500
+	resp, err := http.Get("https://api.ipma.pt/open-data/distrits-islands.json")
+	checkError(err)
+	defer resp.Body.Close()
+
+	var locations IPMA
+	err = json.NewDecoder(resp.Body).Decode(&locations)
+	checkError(err)
+
+	for _, location := range locations.Data {
+		if location.Local == city {
+			return location.GlobalIdLocal
+		}
+	}
+	return 0
 }
 
-func getWeatherForecast(id int, days int) [7]string {
-	// parse json from https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/{id}.json
-	// return array with relevant information
+func getWeatherForecast(id int, days int) IPMA {
+
+	resp, err := http.Get("https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/" + strconv.Itoa(id) + ".json")
+	checkError(err)
+	defer resp.Body.Close()
+
+	var forecasts IPMA
+	err = json.NewDecoder(resp.Body).Decode(&forecasts)
+	checkError(err)
+
+	return forecasts
 }
 
 // missing same day reports
